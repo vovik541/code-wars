f9547fc85f47a447ecc9c9e86fe3014c8aab387f
vovik541 <conanzhill@gmail.com>
2025-05-05 18:37:48 -0700

    Exes and Ohs problem solved

--- src/basicKatas/exesAndOhs/Main.java
+++ src/basicKatas/exesAndOhs/Main.java
@@ -2,6 +2,39 @@ package basicKatas.exesAndOhs;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getXOShortImpl("OzxXzodoX"));
+    }
+
+    public static boolean getXOShortImpl(String str) {
+        return str.chars()
+                .map(Character::toUpperCase)
+                .filter(c -> c == 'O' || c == 'X')
+                .map(c -> c == 'O' ? 1 : -1)
+                .sum() == 0;
+    }
+
+    public static boolean getXO(String str) {
+        String filtered = str.replaceAll("[^oOxX]", "");
+        boolean isEqual;
+
+        if (filtered.length() % 2 == 1) {
+            isEqual = false;
+        } else {
+            filtered = filtered.toUpperCase();
+            int counter = 0;
+
+            for (int i = 0; i < filtered.length(); i++) {
+                char c = filtered.charAt(i);
+                if (c == 'O') {
+                    counter++;
+                } else if (c == 'X') {
+                    counter--;
+                }
+            }
+
+            isEqual = counter == 0;
+        }
+
+        return isEqual;
     }
 }
